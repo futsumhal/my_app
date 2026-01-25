73e7fd13948d19ccac99bf785899e165a4c58967
futsumhal <futsumhal@gmail.com>
2026-01-25 08:09:08 +0300

    connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,9 +7,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.get("/", (req: Request, res: Response) => {
+
   res.send("Hello, World!");
 });
+app.get("/api/hello", (req: Request, res: Response) => {
+
+  res.json({ message: "Hello, World!" } );
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
